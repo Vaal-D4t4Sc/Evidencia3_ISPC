d5e5d9b3fd950e9b89fd3835d8534eb63c8d316e
Numaaaaaaaaa <169181729+Numaaaaaaaaa@users.noreply.github.com>
2024-06-07 21:41:54 -0300

    Update and rename usuario.Py to usuario.py

---
+++ Pseudocodigo modulariza/usuario.py
@@ -0,0 +1,12 @@
+
+def iniciar_sesion():
+    usuario = input("Ingrese su nombre de usuario: ")
+    contrasena = input("Ingrese su contraseña: ")
+    print("Iniciando sesión...")
+
+def registrarse():
+    nombre = input("Ingrese su nombre completo: ")
+    usuario = input("Ingrese su nombre de usuario: ")
+    contrasena = input("Ingrese su contraseña: ")
+    email = input("Ingrese su correo electrónico: ")
+    print("Registrándose...")
