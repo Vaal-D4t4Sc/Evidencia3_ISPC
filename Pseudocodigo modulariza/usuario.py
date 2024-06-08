9f3ddb3b8fe22c6ab53ba673578c8256f8ff17ad
Numaaaaaaaaa <169181729+Numaaaaaaaaa@users.noreply.github.com>
2024-06-07 21:48:41 -0300

    Update usuario.py

--- Pseudocodigo modulariza/usuario.py
+++ Pseudocodigo modulariza/usuario.py
@@ -1,4 +1,4 @@
-USUARIO
+#USUARIO: Contiene las funciones iniciar_sesion() y registrarse() para manejar las operaciones de usuario.
 
 def iniciar_sesion():
     usuario = input("Ingrese su nombre de usuario: ")
